a69e23a1e86d461549f233696090efca5406d267
RaunakChhatwal <RaunakChhatwal001@gmail.com>
2024-11-10 07:03:30 -0500

    migrated to tokio

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use std::{ffi::CString, fs, path::Path};
+use std::{ffi::CString, path::Path};
 use anyhow::{anyhow, bail, Result};
 use nix::sched::{unshare, CloneFlags};
 use nix::mount::{mount, umount2, MntFlags, MsFlags};
+use tokio::fs;
 
-fn subprocess(program: &str, args: &[&str], check: bool) -> Result<String> {
-    let mut command = std::process::Command::new(program);
+async fn subprocess(program: &str, args: &[&str], check: bool) -> Result<String> {
+    let mut command = tokio::process::Command::new(program);
     for arg in args {
         command.arg(arg);
     }
 
-    let output = command.output()?;
+    let output = command.output().await?;
     if check && !output.status.success() {
         bail!("Error running {program} {}: {}.", args.join(" "), String::from_utf8(output.stderr.clone())?);
     }
@@ -17,14 +18,14 @@ fn subprocess(program: &str, args: &[&str], check: bool) -> Result<String> {
     String::from_utf8(output.stdout).map_err(Into::into)
 }
 
-fn get_shell_hook(derivation_path: &str) -> Result<String> {
-    let output = subprocess("nix", &["derivation", "show", &derivation_path], true)?;
+async fn get_shell_hook(derivation_path: &str) -> Result<String> {
+    let output = subprocess("nix", &["derivation", "show", &derivation_path], true).await?;
     let derivation = serde_json::from_str::<serde_json::Value>(&output)?;
     let shell_hook = &derivation[&derivation_path]["env"]["shellHook"];
     shell_hook.as_str().map(str::to_owned).ok_or(anyhow!("Unable to parse derivation for shellHook."))
 }
 
-fn create_ld_so_conf() -> Result<()> {
+async fn create_ld_so_conf() -> Result<()> {
     let ld_so_conf_entries = [
         "/lib",
         "/lib/x86_64-linux-gnu",
@@ -37,10 +38,10 @@ fn create_ld_so_conf() -> Result<()> {
         "/usr/lib/i386-linux-gnu",
         "/usr/lib32",
         "/run/opengl-driver/lib",
-        "/run/opengl-driver-32/lib",
+        "/run/opengl-driver-32/lib"
     ];
 
-    fs::write(Path::new("/etc/ld.so.conf"), ld_so_conf_entries.join("\n")).map_err(Into::into)
+    fs::write(Path::new("/etc/ld.so.conf"), ld_so_conf_entries.join("\n")).await.map_err(Into::into)
 }
 
 fn prepend_entries_in_env(key: &str, additions: &[&str]) {
@@ -59,14 +60,14 @@ fn prepare_env_vars() {
         "/usr/local/bin",
         "/usr/local/sbin",
         "/bin",
-        "/sbin",
+        "/sbin"
     ]);
 
     prepend_entries_in_env("XDG_DATA_DIRS", &[
         "/run/opengl-driver/share",
         "/run/opengl-driver-32/share",
         "/usr/local/share",
-        "/usr/share",
+        "/usr/share"
     ]);
 
     prepend_entries_in_env("ACLOCAL_PATH", &["/usr/share/aclocal"]);
@@ -75,11 +76,9 @@ fn prepare_env_vars() {
     std::env::set_var("LOCALE_ARCHIVE", "/usr/lib/locale/locale-archive");
 }
 
-fn get_fhs() -> Result<std::path::PathBuf> {
-    let shell_nix = &std::env::args().nth(1).ok_or(anyhow!("shell.nix path missing."))?;
-    subprocess("nix-build", &[shell_nix], false)?;      // build the fhs environment
-    let derivation_path = subprocess("nix-instantiate", &[shell_nix], true)?;
-    let shell_hook = get_shell_hook(derivation_path.trim())?;
+async fn get_fhs(shell_nix: &str) -> Result<std::path::PathBuf> {
+    let derivation_path = subprocess("nix-instantiate", &[shell_nix], true).await?;
+    let shell_hook = get_shell_hook(derivation_path.trim()).await?;
 
     let pattern = regex::Regex::new(r"/nix/store/([^-]+)-(.+)-shell-env.drv")?;
     let fhsenv_name = &pattern.captures(&derivation_path)
@@ -92,7 +91,7 @@ fn get_fhs() -> Result<std::path::PathBuf> {
 }
 
 // https://man7.org/linux/man-pages/man7/user_namespaces.7.html
-fn enter_namespace() -> Result<()> {
+async fn enter_namespace() -> Result<()> {
     // TODO: does it matter whether user is root?
     // uid and guid before entering user namespace
     let uid = nix::unistd::Uid::current().as_raw();
@@ -100,9 +99,9 @@ fn enter_namespace() -> Result<()> {
     unshare(CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWUSER)?;  // TODO: handle this error
 
     // restore uid and gid
-    fs::write("/proc/self/uid_map", format!("{uid} {uid} 1"))?;
-    fs::write("/proc/self/setgroups", "deny")?;
-    fs::write("/proc/self/gid_map", format!("{gid} {gid} 1"))?;
+    fs::write("/proc/self/uid_map", format!("{uid} {uid} 1")).await?;
+    fs::write("/proc/self/setgroups", "deny").await?;
+    fs::write("/proc/self/gid_map", format!("{gid} {gid} 1")).await?;
 
     mount(None::<&str>, "/", None::<&str>, MsFlags::MS_SLAVE | MsFlags::MS_REC, None::<&str>)?;
 
@@ -110,7 +109,8 @@ fn enter_namespace() -> Result<()> {
 }
 
 // TODO: is there a practical limit on number of bind mounts?
-fn bind_entries(parent: &Path, target: &Path, exclusions: &[&str]) -> Result<()> {
+async fn bind_entries(parent: &Path, target: &Path, exclusions: &[&str]) -> Result<()> {
+    // TODO: loop asynchronously
     for entry in parent.read_dir()?.collect::<Result<Vec<_>, _>>()? {
         let exclude = exclusions.into_iter().any(|exclusion| entry.file_name().to_str() == Some(exclusion));
         if exclude || target.join(entry.file_name()).exists() {
@@ -118,17 +118,17 @@ fn bind_entries(parent: &Path, target: &Path, exclusions: &[&str]) -> Result<()>
         }
 
         let mut source = entry.path();
-        if fs::symlink_metadata(entry.path())?.file_type().is_symlink() {
-            source = parent.join(fs::read_link(entry.path())?);
+        if fs::symlink_metadata(entry.path()).await?.file_type().is_symlink() {
+            source = parent.join(fs::read_link(entry.path()).await?);
         }
 
         let target = target.join(entry.file_name());
         // if source.is_dir() && !target.exists() {
         if !target.exists() {
             if source.is_dir() {
-                fs::create_dir(&target)?;
+                fs::create_dir(&target).await?;
             } else {
-                fs::write(&target, "")?;
+                fs::write(&target, "").await?;
             }
         }
 
@@ -143,26 +143,26 @@ lazy_static::lazy_static! {
     static ref ROOT: &'static Path = Path::new("/");
 }
 
-fn create_new_root(fhs: &Path) -> Result<std::path::PathBuf> {
+async fn create_new_root(fhs: &Path) -> Result<std::path::PathBuf> {
     let new_root = tempfile::TempDir::new()?.into_path();
     mount(None::<&str>, &new_root, Some("tmpfs"), MsFlags::empty(), None::<&str>)?;
 
-    bind_entries(fhs, &new_root, &["etc"])?;
-    fs::create_dir(new_root.join("etc"))?;
+    bind_entries(fhs, &new_root, &["etc"]).await?;
+    fs::create_dir(new_root.join("etc")).await?;
     // exclude login.defs and pam.d so to not mess with authentication
-    bind_entries(&fhs.join("etc"), &new_root.join("etc"), &["login.defs", "pam.d"])?;
+    bind_entries(&fhs.join("etc"), &new_root.join("etc"), &["login.defs", "pam.d"]).await?;
 
     // let root = Path::new("/");
-    bind_entries(&ROOT, &new_root, &["etc", "tmp"])?;     // TODO: explain why not mount tmp directly
-    bind_entries(&ROOT.join("etc"), &new_root.join("etc"), &[])?;
+    bind_entries(&ROOT, &new_root, &["etc", "tmp"]).await?;     // TODO: explain why not mount tmp directly
+    bind_entries(&ROOT.join("etc"), &new_root.join("etc"), &[]).await?;
 
     Ok(new_root)
 }
 
-fn pivot_root(new_root: &Path) -> Result<()> {
+async fn pivot_root(new_root: &Path) -> Result<()> {
     let put_old = new_root.join(tempfile::TempDir::new()?.into_path().strip_prefix("/")?);
-    fs::create_dir(new_root.join("tmp"))?;
-    bind_entries(&ROOT.join("tmp"), &new_root.join("tmp"), &[])?;
+    fs::create_dir(new_root.join("tmp")).await?;
+    bind_entries(&ROOT.join("tmp"), &new_root.join("tmp"), &[]).await?;
 
     let cwd = std::env::current_dir()?;     // cwd before pivot_root
     nix::unistd::pivot_root(new_root, &put_old)?;
@@ -172,17 +172,24 @@ fn pivot_root(new_root: &Path) -> Result<()> {
     umount2(&ROOT.join(put_old.strip_prefix(new_root)?), MntFlags::MNT_DETACH).map_err(Into::into)
 }
 
-fn main() -> Result<()> {
-    let fhs = get_fhs()?;
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let shell_nix = std::env::args().nth(1).ok_or(anyhow!("shell.nix path missing."))?;
 
-    enter_namespace()?;
-    let new_root = create_new_root(&fhs)?;
-    pivot_root(&new_root)?;
+    // asynchronously build the fhs store path
+    let handle = tokio::spawn(glib::clone!(#[to_owned] shell_nix,
+        async move { subprocess("nix-build", &[&shell_nix], false).await }));
 
-    create_ld_so_conf()?;
+    let fhs = get_fhs(&shell_nix).await?;
+    enter_namespace().await?;
+    handle.await??;     // create_new_root needs the fhs store path built
+    let new_root = create_new_root(&fhs).await?;
+    pivot_root(&new_root).await?;
+
+    create_ld_so_conf().await?;
     prepare_env_vars();
 
     std::env::set_var("PS1", r"\[\e[1;32m\]\u \W> \[\e[0m\]");      // make command prompt green
-    nix::unistd::execv(&CString::new("/bin/bash")?, &[CString::new("bash")?, CString::new("--norc")?])?;
+    nix::unistd::execvp(&CString::new("bash")?, &[CString::new("bash")?, CString::new("--norc")?])?;
     unreachable!();
 }
