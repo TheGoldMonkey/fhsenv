4ddbc2c13ebe5423c9cf03cbd165f23244067f74
RaunakChhatwal <RaunakChhatwal001@gmail.com>
2024-11-12 22:13:02 -0500

    used newuidmapp rather than writing to uid_map

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::{ffi::CString, path::{Path, PathBuf}};
+use std::{ffi::{CString, OsStr}, path::{Path, PathBuf}};
 use anyhow::{anyhow, bail, Context, Result};
-use nix::sched::{unshare, CloneFlags};
+use nix::{sched::{unshare, setns, CloneFlags}, sys::signal, unistd::{execvp, Uid, User}};
 use nix::mount::{mount, umount2, MntFlags, MsFlags};
 use tokio::{fs, process::Command};
 
-async fn subprocess(program: &str, args: &[&str]) -> Result<String> {
+async fn subprocess<I: IntoIterator<Item: AsRef<OsStr>>>(program: &str, args: I) -> Result<String> {
     let output = Command::new(program).args(args).output().await?;
 
     if !output.status.success() {
-        bail!("Error running {program} {}: {}.", args.join(" "), String::from_utf8(output.stderr.clone())?);
+        bail!("Error running {program}: {}.", String::from_utf8(output.stderr.clone())?);
     }
 
-    String::from_utf8(output.stdout.trim_ascii().to_vec()).map_err(Into::into)
+    Ok(String::from_utf8(output.stdout)?.trim().into())
 }
 
 async fn create_ld_so_conf() -> Result<()> {
@@ -44,20 +44,20 @@ fn prepend_entries_in_env(key: &str, additions: &[&str]) {
 
 fn prepare_env_vars() {
     prepend_entries_in_env("PATH", &[
-        "/run/wrappers/bin",
-        "/usr/bin",
-        "/usr/sbin",
         "/usr/local/bin",
         "/usr/local/sbin",
+        "/usr/bin",
+        "/usr/sbin",
         "/bin",
-        "/sbin"
+        "/sbin",
+        "/run/wrappers/bin"
     ]);
 
     prepend_entries_in_env("XDG_DATA_DIRS", &[
-        "/run/opengl-driver/share",
-        "/run/opengl-driver-32/share",
         "/usr/local/share",
-        "/usr/share"
+        "/usr/share",
+        "/run/opengl-driver/share",
+        "/run/opengl-driver-32/share"
     ]);
 
     prepend_entries_in_env("ACLOCAL_PATH", &["/usr/share/aclocal"]);
@@ -94,26 +94,75 @@ async fn get_fhs_path(fhs_definition: &str) -> Result<PathBuf> {
     Ok(fhs.into())
 }
 
+#[derive(Clone, Copy)]
+enum Mapping { Uid, Gid }
+
+async fn read_subuid(mapping: Mapping, username: &str) -> Result<(u32, u32)> {
+    let path = match mapping { Mapping::Uid => "/etc/subuid", Mapping::Gid => "/etc/subgid" };
+    let subuid = std::fs::read_to_string(path).context("Failed to read /etc/subuid.")?;
+    for line in subuid.split('\n') {
+        let [_username, lower_id, range] = line.split(':').collect::<Vec<_>>()[..] else {
+            continue;
+        };
+
+        if _username == username {
+            return Ok((
+                lower_id.parse().context("User has invalid lower_id in /etc/subuid.")?,
+                range.parse().context("User has invalid range in /etc/subuid.")?
+            ));
+        }
+    }
+
+    bail!("{username} has no entry in {path}.");
+}
+
+async fn set_mapping(mapping: Mapping, pid: u32, uid: u32, username: &str) -> Result<String> {
+    let (lower_id, range) = read_subuid(mapping, &username).await?;
+    let args = [
+        pid,
+        0, lower_id, uid,
+        uid, uid, 1,
+        uid + 1, lower_id + uid, range - uid
+    ];
+
+    let mapper = match mapping { Mapping::Uid => "newuidmap", Mapping::Gid => "newgidmap" };
+    subprocess(mapper, args.iter().map(u32::to_string).into_iter()).await
+}
+
 // https://man7.org/linux/man-pages/man7/user_namespaces.7.html
+// https://man7.org/linux/man-pages/man1/newuidmap.1.html
 async fn enter_namespace() -> Result<()> {
     // TODO: does it matter whether user is root?
     // uid and guid before entering user namespace
-    let uid = nix::unistd::Uid::current().as_raw();
+    let uid = Uid::current().as_raw();
+    let username = User::from_uid(Uid::current())
+        .unwrap_or(None).ok_or(anyhow!("Failed to get username from uid."))?.name;
     let gid = nix::unistd::Gid::current().as_raw();
 
-    match unshare(CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWUSER) {
-        Err(nix::errno::Errno::EINVAL) if std::fs::read_dir("/proc/self/task")?.count() > 1 =>
-            bail!("Cannot unshare user namespace: process is multithreaded."),
-        result => result.context("Couldn't create namespace.")?
+    // TODO: explain why spawn separate process to create namespace
+    let mut process = Command::new("unshare").args(&["-U", "sleep", "infinity"]).spawn()
+        .context("Couldn't create namespace.")?;
+    let pid = process.id().ok_or(anyhow!("Namespace parent exited prematurely."))?;
+
+    set_mapping(Mapping::Uid, pid, uid, &username).await.context("Failed to map uid")?;
+    std::fs::write(format!("/proc/{pid}/setgroups"), "deny").context("Couldn't disable setgroups")?;
+    set_mapping(Mapping::Gid, pid, gid, &username).await?;
+
+    // Enter the namespace
+    let ns_path = format!("/proc/{pid}/ns/user");
+    let ns_fd = std::fs::File::open(&ns_path).context(format!("Failed to open {ns_path}."))?;
+    setns(ns_fd, CloneFlags::CLONE_NEWUSER).context(format!("Couldn't enter {ns_path}."))?;
+
+    if let Err(error) = signal::kill(nix::unistd::Pid::from_raw(pid as i32), signal::Signal::SIGKILL) {
+        eprintln!("Failed to kill process {pid}: {error}.");
+    } else if let Err(error) = process.wait().await {
+        eprintln!("Failed to wait for process {pid} to exit: {error}.");
     }
 
-    // restore uid and gid
-    fs::write("/proc/self/uid_map", format!("{uid} {uid} 1")).await.context("Failed to map uid.")?;
-    fs::write("/proc/self/setgroups", "deny").await.context("Couldn't disable setgroups")?;
-    fs::write("/proc/self/gid_map", format!("{gid} {gid} 1")).await.context("Failed to map gid")?;
+    unshare(CloneFlags::CLONE_NEWNS).context("Couldn't flag namespace as mount.")?;
 
     mount(None::<&str>, "/", None::<&str>, MsFlags::MS_SLAVE | MsFlags::MS_REC, None::<&str>)
-        .context("Couldn't set recursive slave mount at root.")?;
+        .context("Failed to make root a slave mount.")?;
 
     Ok(())
 }
@@ -128,7 +177,6 @@ async fn bind_entries(parent: &Path, target: &Path, exclusions: &[&str]) -> Resu
         }
 
         let target = target.join(entry.file_name());
-        // if source.is_dir() && !target.exists() {
         if !target.exists() {
             if entry.path().is_dir() {
                 fs::create_dir(&target).await.context("Failed to create stub directory.")?;
@@ -139,7 +187,7 @@ async fn bind_entries(parent: &Path, target: &Path, exclusions: &[&str]) -> Resu
 
         let flags = MsFlags::MS_BIND | MsFlags::MS_REC;
         mount(Some(&entry.path()), &target, None::<&str>, flags, None::<&str>)  // mount works with files too
-            .context(format!("Failed to mount {entry:?} to {target:?}"))?;
+            .context(format!("Failed to mount {entry:?} to {target:?}."))?;
     }
 
     Ok(())
@@ -158,7 +206,6 @@ async fn create_new_root(fhs_path: &Path) -> Result<PathBuf> {
     // exclude login.defs and pam.d so to not mess with authentication
     bind_entries(&fhs_path.join("etc"), &new_root.join("etc"), &["login.defs", "pam.d"]).await?;
 
-    // let root = Path::new("/");
     bind_entries(&ROOT, &new_root, &["etc", "tmp"]).await?;     // TODO: explain why not mount tmp directly
     bind_entries(&ROOT.join("etc"), &new_root.join("etc"), &[]).await?;
 
@@ -181,7 +228,7 @@ async fn pivot_root(new_root: &Path) -> Result<()> {
 async fn define_fhs(cli: &Cli) -> Result<String> {
     if let Some(packages) = &cli.packages {
         if cli.shell_nix.is_some() {
-            bail!("--packages isn't available when an input file is specified.");
+            bail!("--packages isn't available when the input is provided.");
         }
 
         // TODO: check how nix-shell sanitizes/validates packages input
@@ -195,7 +242,7 @@ async fn define_fhs(cli: &Cli) -> Result<String> {
             }}).env
         "))
     } else {
-        let shell_nix = cli.shell_nix.as_ref().map(PathBuf::as_path).unwrap_or(Path::new("shell.nix"));
+        let shell_nix = cli.shell_nix.as_ref().map(PathBuf::as_path).unwrap_or(Path::new("./shell.nix"));
         if !shell_nix.exists() {
             bail!("{:?} does not exist.", shell_nix.canonicalize()?);
         }
@@ -208,6 +255,21 @@ async fn define_fhs(cli: &Cli) -> Result<String> {
     }
 }
 
+fn enter_shell(cli: Cli) -> Result<()> {
+    let name = CString::new("bash")?;               // TODO: use the default shell rather than bash
+    let entrypoint = cli.run.unwrap_or_else(|| {
+        // make the command prompt green
+        let ps1 = r"\[\e[1;32m\]\u \W> \[\e[0m\]";
+        let set_ps1 = format!("export PS1=\"{ps1}\"");
+        // https://serverfault.com/questions/368054/
+        format!("bash --init-file <(echo \"{}\")", set_ps1.replace("\"", "\\\""))
+    });
+    execvp(&name, &[&name, &CString::new("-c")?, &CString::new(entrypoint)?])
+        .context("execvp into bash failed.")?;
+
+    unreachable!();
+}
+
 #[derive(clap::Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
@@ -217,7 +279,7 @@ struct Cli {
     packages: Option<Vec<String>>,
 
     #[arg(long)]
-    run: Option<String>,
+    run: Option<String>
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -233,17 +295,5 @@ async fn main() -> Result<()> {
     create_ld_so_conf().await?;
     prepare_env_vars();
 
-    if let Some(command) = cli.run {
-        let name = CString::new(command.clone())?;
-        nix::unistd::execvp(&name, &[&name]).context(format!("execvp into {command} failed."))?;
-    } else {
-        let name = CString::new("bash")?;               // TODO: use the user's default shell rather than bash
-        let ps1 = r"\[\e[1;32m\]\u \W> \[\e[0m\]";      // make the command prompt green
-        let set_ps1 = format!("export PS1=\"{ps1}\"");
-        // https://serverfault.com/questions/368054/
-        let entrypoint = format!("bash --init-file <(echo \"{}\")", set_ps1.replace("\"", "\\\""));
-        nix::unistd::execvp(&name, &[&name, &CString::new("-c")?, &CString::new(entrypoint)?])
-            .context("execvp into bash failed.")?;
-    }
-    unreachable!();
+    enter_shell(cli)
 }
